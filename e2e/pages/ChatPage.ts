efc33efb260b1489c65e9d18ff7642efbf1dbe28
Rodrigo Marchi <orodrigomarchi@gmail.com>
2026-07-29 10:29:19 -0300

    style(e2e): format chat page

--- e2e/pages/ChatPage.ts
+++ e2e/pages/ChatPage.ts
@@ -1,7 +1,12 @@
 import { Page, Locator, expect } from "@playwright/test";
 
 export type AddressBookControlType =
-  "all" | "messages" | "pms" | "actions" | "notices" | "invites";
+  | "all"
+  | "messages"
+  | "pms"
+  | "actions"
+  | "notices"
+  | "invites";
 
 type ChannelCentralTab = "general" | "modes" | "access_lists" | "registration";
 
@@ -9,7 +14,9 @@ type ChannelCentralTab = "general" | "modes" | "access_lists" | "registration";
 type ChannelCentralAccessList = "bans" | "ban_exceptions" | "invite_exceptions";
 
 type ChannelCentralModeLabel =
-  "Moderated (+m)" | "Invite Only (+i)" | "Topic Lock (+t)";
+  | "Moderated (+m)"
+  | "Invite Only (+i)"
+  | "Topic Lock (+t)";
 
 function escapeRegExp(value: string): string {
   return value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
